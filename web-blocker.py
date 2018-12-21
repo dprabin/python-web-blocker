4453d4596791ecc873082b40c1ef28755446eb29
Prabin Babu Dhakal <dprabin@yahoo.com>
2018-12-22 01:10:23 +0545

    added loop and wait

--- web-blocker.py
+++ web-blocker.py
@@ -1,4 +1,8 @@
-
+fc?
 hosts_path = r"c:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
-website_list = []"www.facebook.com","facebook.com","live.com","hotmail.com"]
+website_list = ["www.facebook.com","facebook.com","live.com","hotmail.com"]
+
+while True:
+    print(1)
+    time.sleep(5)
